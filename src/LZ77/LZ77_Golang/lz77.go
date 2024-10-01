a5dd2311fda9877db9d992d6f30468a6fbe02246
Mohanad Ahmed <mhndahmd031@gmail.com>
2024-10-01 20:07:14 +0300

    refactor: change the order of the functions

--- src/LZ77/LZ77_Golang/lz77.go
+++ src/LZ77/LZ77_Golang/lz77.go
@@ -10,6 +10,77 @@ type triple struct {
 var LookaheadSize = 6
 var SearchWindowSize = 7
 
+func main() {
+	testString := "ffcabracadabrarrarradff"
+	result := encode(testString)
+	for _, t := range result {
+		fmt.Println(*t)
+	}
+	fmt.Println("decoded :", decode(result))
+	fmt.Println("original:", testString)
+	fmt.Println("=====================================")
+	testString2 := "rararbcrarbc"
+	result = encode(testString2)
+	for _, t := range result {
+		fmt.Println(*t)
+	}
+	fmt.Println("decoded :", decode(result))
+	fmt.Println("original:", testString2)
+	fmt.Println("=====================================")
+	testString3 := "xyxyxyxyxyxyzzzzzzzz"
+	result = encode(testString3)
+	for _, t := range result {
+		fmt.Println(*t)
+	}
+	fmt.Println("decoded :", decode(result))
+	fmt.Println("original:", testString3)
+}
+
+func encode(s string) []*triple {
+	var triples []*triple
+	lookaheadPtr, searchPtr := 0, 0
+
+	for lookaheadPtr < len(s) {
+		// use this if you want to have look-ahead limit
+		offset, length, codeword := findTheLongestMatch(searchPtr, lookaheadPtr, LookaheadSize, &s)
+
+		// use this if you do not want look-ahead limit
+		// offset, length, codeword := findTheLongestMatch(searchPtr, lookaheadPtr, len(s)-lookaheadPtr, &s)
+
+		triples = append(triples, &triple{offset: offset, length: length, codeword: codeword})
+
+		lookaheadPtr += length + 1
+		searchPtr += max(0, lookaheadPtr-SearchWindowSize) // use this if you want to have search limit
+	}
+
+	return triples
+}
+
+func findTheLongestMatch(searchPtr, lookaheadPtr, lookaheadSize int, pattern *string) (int, int, string) {
+	s := *pattern
+	maxLength := 0
+	offset := 0
+	nextCharacter := string(s[lookaheadPtr])
+
+	// looking for a match in the search window
+	for ; searchPtr < lookaheadPtr; searchPtr++ {
+		// not a match, skip
+		if s[searchPtr] != s[lookaheadPtr] {
+			continue
+		}
+
+		length, char := findMatchLengthAndCodeword(searchPtr, lookaheadPtr, lookaheadSize, pattern)
+
+		if length >= maxLength {
+			maxLength = length
+			offset = lookaheadPtr - searchPtr
+			nextCharacter = char
+		}
+	}
+
+	return offset, maxLength, nextCharacter
+}
+
 func findMatchLengthAndCodeword(p1, p2, lookaheadSize int, input *string) (int, string) {
 	s := *input
 	nextCharacter := ""
@@ -49,51 +120,6 @@ func findMatchLengthAndCodeword(p1, p2, lookaheadSize int, input *string) (int, 
 	return length, nextCharacter
 }
 
-func findTheLongestMatch(searchPtr, lookaheadPtr, lookaheadSize int, pattern *string) (int, int, string) {
-	s := *pattern
-	maxLength := 0
-	offset := 0
-	nextCharacter := string(s[lookaheadPtr])
-
-	// looking for a match in the search window
-	for ; searchPtr < lookaheadPtr; searchPtr++ {
-		// not a match, skip
-		if s[searchPtr] != s[lookaheadPtr] {
-			continue
-		}
-
-		length, char := findMatchLengthAndCodeword(searchPtr, lookaheadPtr, lookaheadSize, pattern)
-
-		if length >= maxLength {
-			maxLength = length
-			offset = lookaheadPtr - searchPtr
-			nextCharacter = char
-		}
-	}
-
-	return offset, maxLength, nextCharacter
-}
-
-func encode(s string) []*triple {
-	var triples []*triple
-	lookaheadPtr, searchPtr := 0, 0
-
-	for lookaheadPtr < len(s) {
-		// use this if you want to have look-ahead limit
-		offset, length, codeword := findTheLongestMatch(searchPtr, lookaheadPtr, LookaheadSize, &s)
-
-		// use this if you do not want look-ahead limit
-		// offset, length, codeword := findTheLongestMatch(searchPtr, lookaheadPtr, len(s)-lookaheadPtr, &s)
-
-		triples = append(triples, &triple{offset: offset, length: length, codeword: codeword})
-
-		lookaheadPtr += length + 1
-		searchPtr += max(0, lookaheadPtr-SearchWindowSize) // use this if you want to have search limit
-	}
-
-	return triples
-}
-
 func decode(t []*triple) string {
 	result := ""
 
@@ -107,30 +133,4 @@ func decode(t []*triple) string {
 	}
 
 	return result
-}
-
-func main() {
-	testString := "ffcabracadabrarrarradff"
-	result := encode(testString)
-	for _, t := range result {
-		fmt.Println(*t)
-	}
-	fmt.Println("decoded :", decode(result))
-	fmt.Println("original:", testString)
-	fmt.Println("=====================================")
-	testString2 := "rararbcrarbc"
-	result = encode(testString2)
-	for _, t := range result {
-		fmt.Println(*t)
-	}
-	fmt.Println("decoded :", decode(result))
-	fmt.Println("original:", testString2)
-	fmt.Println("=====================================")
-	testString3 := "xyxyxyxyxyxyzzzzzzzz"
-	result = encode(testString3)
-	for _, t := range result {
-		fmt.Println(*t)
-	}
-	fmt.Println("decoded :", decode(result))
-	fmt.Println("original:", testString3)
 }
