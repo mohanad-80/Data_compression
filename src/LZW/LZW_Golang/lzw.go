08da27fc39904317034b3528500a89a1edd57b9a
Mohanad Ahmed <mhndahmd031@gmail.com>
2024-10-14 06:04:41 +0300

    refactor: reorder the functions in a logical order and remove commented out code

--- src/LZW/LZW_Golang/lzw.go
+++ src/LZW/LZW_Golang/lzw.go
@@ -2,85 +2,6 @@ package main
 
 import "fmt"
 
-func createAlphabetDict(pattern string) []string {
-	dict := []string{}
-
-	for _, char := range pattern {
-		if findIn(dict, string(char)) == 0 {
-			dict = append(dict, string(char))
-		}
-	}
-
-	return dict
-}
-
-func encode(pattern string) ([]int, []string) {
-	alphabetDict := createAlphabetDict(pattern)
-	// alphabetDict := []string{"/", "a", "b", "o", "w"}
-	dict := alphabetDict
-	output := []int{}
-	currentPatternToCheck := string(pattern[0])
-	latestFoundIdx := 0
-
-	for i := 1; ; i++ {
-		foundIdx := findIn(dict, currentPatternToCheck)
-
-		if foundIdx == 0 {
-			output = append(output, latestFoundIdx)
-			dict = append(dict, currentPatternToCheck)
-			currentPatternToCheck = string(pattern[i-1])
-			i--
-		} else if i == len(pattern) {
-			// in the case were we reach the end of the pattern
-			// and the currentPatternToCheck is in the dict so we
-			// add its index to the output
-			output = append(output, foundIdx)
-			break
-		} else {
-			currentPatternToCheck += string(pattern[i])
-			latestFoundIdx = foundIdx
-		}
-	}
-
-	return output, alphabetDict
-}
-
-func decode(output []int, alphabetDict []string) string {
-	dict := alphabetDict
-	result := alphabetDict[output[0]-1]
-	previousStep := result
-	currentStep := ""
-
-	for i := 1; i < len(output); i++ {
-		index := output[i] - 1
-		if index >= len(dict) {
-			// if the index is not known in the dict yet we construct the
-			// unknown by concatenating the previous step and the first
-			// symbol in the previous step
-			currentStep = string(previousStep[0])
-			result += previousStep + currentStep
-			dict = append(dict, previousStep+string(currentStep[0]))
-			previousStep += currentStep
-		} else {
-			currentStep = dict[index]
-			result += currentStep
-			dict = append(dict, previousStep+string(currentStep[0]))
-			previousStep = currentStep
-		}
-	}
-
-	return result
-}
-
-func findIn(a []string, element string) int {
-	for i, v := range a {
-		if v == element {
-			return i + 1
-		}
-	}
-	return 0
-}
-
 func main() {
 	testString := "ffcabracadabrarrarradff"
 	result, alphabetDict := encode(testString)
@@ -125,4 +46,82 @@ func main() {
 	}
 	fmt.Println("decoded :", decode(result, alphabetDict))
 	fmt.Println("original:", testString4)
+}
+
+func encode(pattern string) ([]int, []string) {
+	alphabetDict := createAlphabetDict(pattern)
+	dict := alphabetDict
+	output := []int{}
+	currentPatternToCheck := string(pattern[0])
+	latestFoundIdx := 0
+
+	for i := 1; ; i++ {
+		foundIdx := findIn(dict, currentPatternToCheck)
+
+		if foundIdx == 0 {
+			output = append(output, latestFoundIdx)
+			dict = append(dict, currentPatternToCheck)
+			currentPatternToCheck = string(pattern[i-1])
+			i--
+		} else if i == len(pattern) {
+			// in the case were we reach the end of the pattern
+			// and the currentPatternToCheck is in the dict so we
+			// add its index to the output
+			output = append(output, foundIdx)
+			break
+		} else {
+			currentPatternToCheck += string(pattern[i])
+			latestFoundIdx = foundIdx
+		}
+	}
+
+	return output, alphabetDict
+}
+
+func createAlphabetDict(pattern string) []string {
+	dict := []string{}
+
+	for _, char := range pattern {
+		if findIn(dict, string(char)) == 0 {
+			dict = append(dict, string(char))
+		}
+	}
+
+	return dict
+}
+
+func findIn(a []string, element string) int {
+	for i, v := range a {
+		if v == element {
+			return i + 1
+		}
+	}
+	return 0
+}
+
+func decode(output []int, alphabetDict []string) string {
+	dict := alphabetDict
+	result := alphabetDict[output[0]-1]
+	previousStep := result
+	currentStep := ""
+
+	for i := 1; i < len(output); i++ {
+		index := output[i] - 1
+		if index >= len(dict) {
+			// if the index is not known in the dict yet we construct the
+			// unknown by concatenating the previous step and the first
+			// symbol in the previous step
+			currentStep = string(previousStep[0])
+			result += previousStep + currentStep
+			dict = append(dict, previousStep+string(currentStep[0]))
+			previousStep += currentStep
+		} else {
+			currentStep = dict[index]
+			result += currentStep
+			dict = append(dict, previousStep+string(currentStep[0]))
+			previousStep = currentStep
+		}
+	}
+
+	return result
 }
